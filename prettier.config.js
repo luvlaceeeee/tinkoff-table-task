307545fe0494022b4e3c91d271d11e6ebb85f332
luvlaceeeee <nikitan2002@gmail.com>
2023-08-23 18:56:22 +0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    proseWrap: 'always',
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    endOfLine: 'lf',
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+};
